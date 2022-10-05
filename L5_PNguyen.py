5f4e47aaacd9126689a0ca4ae9e4f9e3070f0960
ppeternnguyen <89424139+2SikNinja@users.noreply.github.com>
2022-10-05 16:57:26 -0700

    made rainbow rotating squares

--- L5_PNguyen.py
+++ L5_PNguyen.py
@@ -1,9 +1,18 @@
 #Authors: Peter Nguyen
 #Assignment: Lab #5
 #Completed (or last revision): 10/06/2022
-
-import turtle
-
-def function(userInput):
-    my_turtle = turtle.turtle()
-    
+from turtle import *
+from random import *
+x = 1
+bgcolor('black')
+speed(0)
+while x < 600:
+    r = randint(0,255) 
+    g = randint(0,255)  
+    b = randint(0,255) 
+    colormode(255)
+    pencolor(r,g,b)
+    fd(100 + x)
+    rt(90.991)
+    x+=2
+done()
